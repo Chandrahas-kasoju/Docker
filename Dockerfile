75c5f2f490dcad6930e49865954f995ff0b79c67
Chandrahas Kasoju <kasoju.chandrahas@gmail.com>
2025-02-17 12:22:39 +0100

    first_commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Use the official ROS 2 Humble base image
+FROM ros:humble-ros-base
+
+# Set environment variables
+ENV ROS_DISTRO=humble
+ENV DEBIAN_FRONTEND=noninteractive
+
+# Install necessary dependencies
+RUN apt update && apt install -y \
+    python3-colcon-common-extensions \
+    git \
+    python3-rosdep \
+    && rm -rf /var/lib/apt/lists/*
+
+
+WORKDIR /home/micro_ros
+
+COPY scripts /home/micro_ros
+
+RUN chmod +x ./micro_ros.sh
+
+RUN /bin/bash -c "source /opt/ros/humble/setup.bash && ./micro_ros.sh"
+
+# Set entrypoint to source ROS 2
+ENTRYPOINT ["/bin/bash", "-c", "source /opt/ros/humble/setup.bash && exec bash"]
+
+
+
---
+++ micro_ros.sh
@@ -0,0 +1,36 @@
+# steps to install micro_ros
+
+#!/bin/bash
+
+# Source the ROS 2 installation
+source /opt/ros/humble/setup.bash
+
+# Create a workspace and download the micro-ROS tools
+mkdir microros_ws
+cd microros_ws
+git clone -b $ROS_DISTRO https://github.com/micro-ROS/micro_ros_setup.git src/micro_ros_setup
+
+# Update dependencies using rosdep
+sudo apt update && rosdep update
+rosdep install --from-paths src --ignore-src -y
+
+# Install pip
+sudo apt-get install python3-pip
+
+# Build micro-ROS tools and source them
+colcon build
+source install/local_setup.bash
+
+# Create firmware step
+ros2 run micro_ros_setup create_firmware_ws.sh host
+
+# Build step
+ros2 run micro_ros_setup build_firmware.sh
+source install/local_setup.bash
+
+# Download micro-ROS-Agent packages
+ros2 run micro_ros_setup create_agent_ws.sh
+
+# Build agent step
+ros2 run micro_ros_setup build_agent.sh
+source install/local_setup.bash
